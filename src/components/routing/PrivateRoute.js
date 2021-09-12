56a7d7467eab655dfc4d25e899e4411319829fe8
Ezhilarasi <tezzhilarasi@gmail.com>
2021-09-12 15:04:17 +0530

    c

--- src/components/routing/PrivateRoute.js
+++ src/components/routing/PrivateRoute.js
@@ -10,7 +10,7 @@ const PrivateRoute = ({ component: Component,...rest }) => {
         localStorage.getItem("authToken") ? (
           <Component {...props} />
         ) : (
-          <Redirect to="https://crmapp-fullstack.herokuapp.com/api/auth/login"/>
+          <Redirect to="/login"/>
         )
       }
     />
