e3a0053b73ea1a3815ade74b4fa6394ed79da313
Ezhilarasi <tezzhilarasi@gmail.com>
2021-09-12 14:56:39 +0530

    final comit

--- src/components/screens/ResetPasswordScreen.js
+++ src/components/screens/ResetPasswordScreen.js
@@ -30,7 +30,7 @@ const ResetPasswordScreen = ({ match }) => {
 
     try {
       const { data } = await axios.put(
-        `/api/auth/resetpassword/${match.params.resetToken}`,
+        `https://crmapp-fullstack.herokuapp.com/api/auth/resetpassword/${match.params.resetToken}`,
         {
           password,
         },
